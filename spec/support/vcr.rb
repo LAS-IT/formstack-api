958fc071b6d0141dedf90d3617c0c70e6bd8e80d
Mauro Morales <contact@mauromorales.com>
2014-11-14 16:17:12 +0100

    Add missing functionality

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,13 +4,25 @@ require 'rspec'
 ACCESS_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 FORM_ID = 0000000
 INACCESSIBLE_FORM_ID = 0000001
+DELETE_SUBMISSION_FORM = 0000000
+SUBMIT_FORM_ID = DELETE_SUBMISSION_FORM
+CREATE_FIELD_FORM_ID = DELETE_SUBMISSION_FORM
+SUBMISSION_DETAILS_ID = 000000000
+SUBMISSION_DETAILS_FORM_ID = 0000000
+EDIT_SUBMISSION_FIELD_ID = 00000000
+EDIT_SUBMISSION_ARRAY_FIELD_ID = 00000000
 
 VCR.configure do |c|
     c.cassette_library_dir = 'spec/vcr'
     c.hook_into :webmock # or :fakeweb
     c.filter_sensitive_data('<ACCESS_TOKEN>') { ACCESS_TOKEN }
-    c.filter_sensitive_data('<FORM_ID>') { FORM_ID }
+    c.filter_sensitive_data('<FORM_ID>') { COPY_FORM_ID }
     c.filter_sensitive_data('<INACCESSIBLE_FORM_ID>') { INACCESSIBLE_FORM_ID }
+    c.filter_sensitive_data('<SUBMISSION_DETAILS_ID>') { SUBMISSION_DETAILS_ID }
+    c.filter_sensitive_data('<SUBMISSION_DETAILS_FORM_ID>') { SUBMISSION_DETAILS_FORM_ID }
+    c.filter_sensitive_data('<DELETE_SUBMISSION_FORM>') { DELETE_SUBMISSION_FORM }
+    c.filter_sensitive_data('<EDIT_SUBMISSION_FIELD_ID>') { EDIT_SUBMISSION_FIELD_ID }
+    c.filter_sensitive_data('<EDIT_SUBMISSION_ARRAY_FIELD_ID>') { EDIT_SUBMISSION_ARRAY_FIELD_ID }
     c.default_cassette_options = { record: :new_episodes }
     c.configure_rspec_metadata!
 end
--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -2,11 +2,19 @@ require_relative '../lib/formstack/api'
 
 RSpec.describe Formstack::API do
 
-  subject(:wrapper) { Formstack::API.new(access_token) }
   let(:access_token) { ACCESS_TOKEN }
-  let(:valid_form_id) { FORM_ID }
+  let(:copy_form_id) { COPY_FORM_ID }
   let(:invalid_form_id) { 1234 }
   let(:inaccessible_form_id) { INACCESSIBLE_FORM_ID }
+  let(:delete_submission_form) { DELETE_SUBMISSION_FORM }
+  let(:submit_form_id) { SUBMIT_FORM_ID }
+  let(:create_field_form_id) { CREATE_FIELD_FORM_ID }
+  let(:submission_details_id) { SUBMISSION_DETAILS_ID }
+  let(:submission_details_form_id) { SUBMISSION_DETAILS_FORM_ID }
+  let(:edit_submission_field_id) { EDIT_SUBMISSION_FIELD_ID }
+  let(:edit_submission_array_field_id) { EDIT_SUBMISSION_ARRAY_FIELD_ID }
+
+  subject(:wrapper) { Formstack::API.new(access_token) }
 
   describe '#request', :vcr do
 
@@ -38,7 +46,7 @@ RSpec.describe Formstack::API do
 
   describe '#form_details', :vcr do
     it 'reads a form name' do
-      form = wrapper.form_details(valid_form_id)
+      form = wrapper.form_details(copy_form_id)
       expect(form['name']).to eq('Test Form')
     end
 
@@ -61,8 +69,8 @@ RSpec.describe Formstack::API do
 
   describe '#copy_form', :vcr do
     it 'adds COPY at the end of the form name' do
-      form = wrapper.form_details(valid_form_id)
-      copied_form = wrapper.copy_form(valid_form_id)
+      form = wrapper.form_details(copy_form_id)
+      copied_form = wrapper.copy_form(copy_form_id)
       expect(copied_form['name']).to match(/^#{form['name']} - COPY/)
     end
 
@@ -79,10 +87,10 @@ RSpec.describe Formstack::API do
 
   describe '#form_submissions', :vcr do
     it 'gets submissions default' do
-      form = wrapper.form_details(valid_form_id)
+      form = wrapper.form_details(submit_form_id)
       submissions_count = form['submissions'].to_i > 25 ? 25 : form['submissions'].to_i
 
-      submissions = wrapper.form_submissions(valid_form_id)
+      submissions = wrapper.form_submissions(submit_form_id)
       expect(submissions_count).to eq(submissions.count)
     end
 
@@ -91,55 +99,225 @@ RSpec.describe Formstack::API do
     end
 
     it 'raises an error if min time is invalid' do
-      expect{wrapper.form_submissions(valid_form_id, '', 'BAD')}.to raise_exception('Invalid value for minTime parameter')
+      expect{wrapper.form_submissions(submit_form_id, '', 'BAD')}.to raise_exception('Invalid value for minTime parameter')
     end
 
     it 'raises an error if max time is invalid' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', 'BAD')}.to raise_exception('Invalid value for maxTime parameter')
+      expect{wrapper.form_submissions(submit_form_id, '', '', 'BAD')}.to raise_exception('Invalid value for maxTime parameter')
     end
 
     it 'raises an error for a search mismatch' do
       field_ids = [1, 2, 3]
       field_values = ['test-1', 'test-2']
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', field_ids, field_values)}.to raise_exception('You must have a one to one relationship')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', field_ids, field_values)}.to raise_exception('You must have a one to one relationship')
     end
 
     it 'raises an error for a search mismatch reversed' do
       field_ids = [1, 2]
       field_values = ['test-1', 'test-2', 'test-3']
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', field_ids, field_values)}.to raise_exception('You must have a one to one relationship')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', field_ids, field_values)}.to raise_exception('You must have a one to one relationship')
     end
 
     it 'raises an error if field ids are not numeric' do
       field_ids = [1, 'two']
       field_values = ['test-1', 'test-2']
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', field_ids, field_values)}.to raise_exception('Field IDs must be numeric')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', field_ids, field_values)}.to raise_exception('Field IDs must be numeric')
     end
 
     it 'raises an error if per_page is not numeric' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', [], [], 1, 'fail')}.to raise_exception('The perPage value must be numeric')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 'fail')}.to raise_exception('The perPage value must be numeric')
     end
 
     it 'raises an error if requests per page are less than 1' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', [], [], 1, 0)}.to raise_exception('You can only retrieve a minimum of 1 and maximum of 100 submissions per request')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 0)}.to raise_exception('You can only retrieve a minimum of 1 and maximum of 100 submissions per request')
     end
 
     it 'raises an error if requests per page are more than 100' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', [], [], 1, 101)}.to raise_exception('You can only retrieve a minimum of 1 and maximum of 100 submissions per request')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 101)}.to raise_exception('You can only retrieve a minimum of 1 and maximum of 100 submissions per request')
     end
 
     it 'raises an error if page_number is not numeric' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', [], [], 'fail')}.to raise_exception('The pageNumber value must be numeric')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', [], [], 'fail')}.to raise_exception('The pageNumber value must be numeric')
     end
 
-    it 'raises an error if page number is not working'
+    it 'raises an error if page number is not working' do
+      page1 = wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 1)
+      page2 = wrapper.form_submissions(submit_form_id, '', '', '', [], [], 2, 1)
+
+      expect(page1).not_to eq(page2)
+    end
 
     it 'raises an error if the sort parameter is invalid' do
-      expect{wrapper.form_submissions(valid_form_id, '', '', '', [], [], 1, 100, 'fail')}.to raise_exception('The sort parameter must be ASC or DESC')
+      expect{wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 100, 'fail')}.to raise_exception('The sort parameter must be ASC or DESC')
     end
 
-    it 'returns false with no data'
-    it 'returns true with data'
+    it 'returns no data by default' do
+      submission = wrapper.form_submissions(submit_form_id).first
 
+      expect(submission.include?('data')).to eq(false)
+    end
+
+    it 'returns data if specified' do
+      submission = wrapper.form_submissions(submit_form_id, '', '', '', [], [], 1, 100, 'DESC', true).first
+
+      expect(submission.include?('data')).to eq(true)
+    end
+
+  end
+
+  describe '#submit_form', :vcr do
+    let(:submit_form_field_ids) { [28705506, 28706107, 28706112] }
+    let(:submit_form_field_values) { ['short-answer-test', {'first' => 'first-test', 'last' => 'last-test'}, 'this is a long answer field'] }
+
+    it 'creates a submission into a form' do
+      response = wrapper.submit_form(submit_form_id, submit_form_field_ids, submit_form_field_values)
+      expect(response['form'].to_i).to eq(submit_form_id)
+
+      response['data'].each do |row|
+        value = submit_form_field_values.to_a[submit_form_field_ids.index(row['field'].to_i)]
+        if value.class == Hash
+          clean_hash = {}
+          row['value'].to_s.split(/\n/).each do |a|
+            key, val = a.split(' = ')
+            clean_hash[key] = val
+          end
+          expect(clean_hash).to eq(value)
+        else
+          expect(row['value']).to eq(value)
+        end
+      end
+    end
+
+    it 'raises an error if the form id is non numeric' do
+      expect{wrapper.submit_form('fail')}.to raise_exception('Form ID must be numeric')
+    end
+
+    it 'raises an eror if the timestamp format is invalid' do
+      expect{wrapper.submit_form(submit_form_id, [], [], 'this is not a valid date/time string')}.to raise_exception('You must use a valid Date/Time string formatted in YYYY-MM-DD HH:MM:SS')
+    end
+
+    it 'raises an error for a data array mismatch' do
+      field_ids = [1, 2, 3]
+      field_values = ['a', 'b']
+      expect{wrapper.submit_form(submit_form_id, field_ids, field_values)}.to raise_exception('There must be a one-to-one relationship between Field IDs and their values')
+    end
+
+    it 'raises an error for a data array mismatch reversed' do
+      field_ids = [1, 2]
+      field_values = ['a', 'b', 'c']
+      expect{wrapper.submit_form(submit_form_id, field_ids, field_values)}.to raise_exception('There must be a one-to-one relationship between Field IDs and their values')
+    end
+
+    it 'raises an error if field ids are not numeric' do
+      field_ids = ['one']
+      field_values = ['a']
+      expect{wrapper.submit_form(submit_form_id, field_ids, field_values)}.to raise_exception('Field IDs must be numeric')
+    end
+
+    it 'uploads'
+  end
+
+  describe '#get_submission_details', :vcr do
+
+    it 'gets the right submission from the right form' do
+      submission = wrapper.get_submission_details(submission_details_id)
+      expect(submission['form'].to_i).to eq(submission_details_form_id)
+    end
+
+    it 'raises an error if submission id is not numeric' do
+      expect{wrapper.get_submission_details('FAIL')}.to raise_exception('Submission ID must be numeric')
+    end
+  end
+
+  describe '#edit_submission_data', :vcr do
+
+    let(:value) { "test#{Time.now}" }
+    let(:hash_value) { { 'first' => "#{value}-first", 'last' => "#{value}-last" } }
+    let(:edit_submission_id) { get_editable_submission_id }
+
+    it 'edits a submission succesfully' do
+
+      response = wrapper.edit_submission_data(edit_submission_id,
+                                              [edit_submission_field_id, edit_submission_array_field_id],
+                                              [value, hash_value])
+
+      expect(response['success'].to_i).to eq(1)
+      expect(response['id'].to_i).to eq(edit_submission_id)
+
+      submission = wrapper.get_submission_details(edit_submission_id)
+
+      submission['data'].each do |row|
+        if row['field'] == edit_submission_field_id
+          expect(row['value']).to eq(value)
+        elsif row['field'] == edit_submission_array_field_id
+          expect(row['value']).to eq(hash_value)
+        end
+      end
+    end
+
+    it 'raises an exception when the submission id is non numeric' do
+      expect{ wrapper.edit_submission_data('FAIL') }.to raise_exception('Submission ID must be numeric')
+    end
+
+    it 'raises an exception with a bad timestamp' do
+      expect{
+        wrapper.edit_submission_data(edit_submission_id, [], [], 'Bad Date String')
+      }.to raise_exception('You must use a valid Date/Time string formatted in YYYY-MM-DD HH:MM:SS')
+    end
+
+    it 'raises an exception when data array and ids don\'t match' do
+      expect{
+        wrapper.edit_submission_data(edit_submission_id, [1,2,3], ['test-1', 'test-2'], 'Bad Date String')
+      }.to raise_exception('You must use a valid Date/Time string formatted in YYYY-MM-DD HH:MM:SS')
+    end
+
+    it 'raises an exception when data array and ids don\'t match reversed' do
+      expect{
+        wrapper.edit_submission_data(edit_submission_id, [1,2], ['test-1', 'test-2', 'test-3'], 'Bad Date String')
+      }.to raise_exception('You must use a valid Date/Time string formatted in YYYY-MM-DD HH:MM:SS')
+    end
+
+    it 'raises an exception if fields are non numeric' do
+      expect{
+        wrapper.edit_submission_data(edit_submission_id, ['fail'], ['test-1'])
+      }.to raise_exception('Field IDs must be numeric')
+    end
+  end
+
+  describe '#delete_submission', :vcr do
+    it 'deletes a submission' do
+      submission_to_delete = get_editable_submission_id
+      response = wrapper.delete_submission(submission_to_delete)
+      expect(response['success'].to_i).to eq(1)
+      expect(response['id'].to_i).to eq(submission_to_delete)
+    end
+
+    it 'raises an exception with a non numeric id' do
+      expect{wrapper.delete_submission('FAIL')}.to raise_exception('Submission ID must be numeric')
+    end
+  end
+
+  describe '#create_field', :vcr do
+    it 'creates a text field' do
+      field_name = "Test Field #{Time.now}"
+      field = wrapper.create_field(create_field_form_id, 'text', field_name)
+
+      expect(field['label']).to match(/Test Field/)
+      expect(field['type']).to eq('text')
+    end
+
+    it 'creates a field with embeded code' do
+      attributes = { 'text' => '<script type="text/javascript">alert(\'Woop there it is \');</script>' }
+
+      field = wrapper.create_field(create_field_form_id, 'embed', '', false, '', false, attributes)
+
+      expect(field['type']).to eq('embed')
+    end
+  end
+
+  # describe '#', :vcr => { :record => :all } do
+
+  def get_editable_submission_id
+    wrapper.form_submissions(delete_submission_form, '', '', '', [], [], 1, 100, 'ASC').first['id'].to_i
   end
 end
